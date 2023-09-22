5807bb75291e6bd066eb166bf51ccbae38ad395b
Angelo Leva <angelokta7@gmail.com>
2023-09-22 09:19:01 -0500

    added og data

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Angelo Leva";
-export const SITE_DESCRIPTION = "Portfolio";
+export const SITE_DESCRIPTION = "This is my portfolio";
