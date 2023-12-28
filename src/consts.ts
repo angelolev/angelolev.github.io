f5ac4e85e146d9657e40ec6bd567b98d4286480b
Angelo Leva <angelokta7@gmail.com>
2023-12-27 22:08:28 -0500

    chore: update meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Angelo Leva";
-export const SITE_DESCRIPTION = "This is my portfolio";
+export const SITE_DESCRIPTION =
+  "Front end | Enjoying bringing ideas to life in the browser | Building high-performance web apps with Javascript and CSS";
