550e8b92354b7720dd9b60f8c894a54ae11f87d1
Angelo Leva <angelokta7@gmail.com>
2024-01-10 14:28:52 -0500

    bugfix:update site key

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://angelodev.vercel.app/",
   integrations: [mdx(), sitemap(), tailwind({})],
   i18n: {
     defaultLocale: "en",
